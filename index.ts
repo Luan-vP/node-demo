bc3a955ebdafab8d8ea0494a518344ac75665495
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-12-20 12:24:05 +0000

    Update server port

--- index.ts
+++ index.ts
@@ -5,5 +5,5 @@ app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-const PORT = 5000; // TODO update from Docker env var
+const PORT = process.env.PORT || 5100; // TODO update from Docker env var
 app.listen(PORT);
