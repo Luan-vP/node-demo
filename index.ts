5712ae4a4ac0a9a4d6b8f71e6c9d29ecf841cc75
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-12-25 09:52:56 +0000

    Add healthcheck

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: any) => {
   res.send({ hi: "there" });
+});
+
+app.get("/healthcheck", (req: any, res: any) => {
+  res.send({ status: "ok" });
 });
 
 const PORT = process.env.PORT || 5100; // TODO update from Docker env var
